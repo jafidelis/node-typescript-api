78a81d5e48edb1d1962cfe04aa0701e90894f1e2
José Alexandre Fidelis <jafidelis@gmail.com>
2021-06-15 07:44:03 -0300

    First commit

---
+++ src/util/module-alias.ts
@@ -0,0 +1,9 @@
+import path from 'path';
+import modeAlias from 'module-alias';
+
+const files = path.resolve(__dirname, '../..');
+
+modeAlias.addAliases({
+    '@src': path.join(files, 'src'),
+    "@test": path.join(files, 'test')
+});
