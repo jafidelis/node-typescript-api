74075501b5b9f661bbb36716ed9b7d9a6d717dd4
José Alexandre Fidelis <jafidelis@gmail.com>
2021-06-16 08:55:07 -0300

    Criado teste e mock para chamada da api externa

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,6 @@ module.exports = {
     preset: 'ts-jest',
     moduleNameMapper: {
         '@src/(.*)': '<rootDir>/src/$1',
-        '@test/(.*)': '<rootDir/test/$1>'
+        '@test/(.*)': '<rootDir>/test/$1'
     }
 }
