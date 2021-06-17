f21ba65604acbbccdcb99156169b364c5a98ec61
José Alexandre Fidelis <jafidelis@gmail.com>
2021-06-17 08:39:12 -0300

    normalização dos dados de retorno da api externa

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -6,13 +6,14 @@ import stormGlassNormalizedWeather3HoursFixture from '@test/fixtures/stormglass_
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.12345;
     const lng = 151.2123;
 
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalizedWeather3HoursFixture);
   });
